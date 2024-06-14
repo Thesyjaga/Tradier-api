0241e9214c2c0e97467f06a0b04ca0c74f37ae83
Thesyjaga <167492583+Thesyjaga@users.noreply.github.com>
2024-06-14 01:10:08 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+# Configuration
+TOKEN = 'Enter your token'
+API_BASE_URL = 'https://api.tradier.com/v1/'
+ACCESS_TOKEN = TOKEN
+profile_url = 'https://api.tradier.com/v1//user/profile'
+
+
